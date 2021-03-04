ad305cf7ea882722dbfd721a48b93b71c73650a8
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-03-05 00:20:35 +0900

    음악이 끝나면 자동으로 스킵되도록 수정

--- modules/music/play.js
+++ modules/music/play.js
@@ -3,6 +3,7 @@ const ytdl = require('ytdl-core');
 const db = require('quick.db');
 const { MessageEmbed } = require('discord.js');
 const { play_end } = require('./play_end');
+const { play_score } = require('./play_score');
 
 const { mongourl } = require('../../config.json');
 const { dbset, dbset_music } = require('../functions');
@@ -34,7 +35,7 @@ module.exports = {
                 channel.leave();
                 return await play_end(client, message);
             }
-            var url = ytdl(link, { bitrate: 512000 });
+            var url = ytdl(link, { bitrate: 512000, quality: 'highestaudio' });
             var options = {
                 volume: 0.08
             };
@@ -71,14 +72,79 @@ module.exports = {
                         m.edit(np);
                     });
                 } catch(err) {}
-                const broadcast = client.voice.createBroadcast();
                 channel.join().then(connection => {
-                    broadcast.play(url, options);
-                    connection.play(broadcast);
+                    const dispatcher = connection.play(url, options);
+                    dispatcher.on("finish", () => {
+                        play_anser(data, client);
+                    });
                 });
             } catch(err) {
                 await play_end(client, message);
             }
         });
+
+        // play_anser 에서 살짝 변형된 함수
+        async function play_anser (data, client) {
+            try {
+                await data.save().catch(err => console.log(err));
+                c_anser = '시간초과로 스킵되었습니다.';
+                var skip = data.skip;
+                if (skip == undefined || skip == 0) {
+                    skip = 1;
+                } else {
+                    skip = skip + 1;
+                }
+                data.skip = skip;
+                await data.save().catch(err => console.log(err));
+                var time = 10;
+                var count = data.count;
+                var all_count = data.name.length;
+                var name = data.name[count];
+                var vocal = data.vocal[count];
+                var link = data.link[count];
+                var chack = /(?:http:\/\/|https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?/gi;
+                var yturl = link.replace(chack, '').replace(/(?:&(.+))/gi, '');
+                var list = `음악퀴즈를 종료하시려면 \` ;음악퀴즈 종료 \`를 입력해 주세요.`;
+                var np = new MessageEmbed()
+                    .setTitle(`**정답 : ${name}**`)
+                    .setURL(`${link}`)
+                    .setDescription(`**가수 : ${vocal}**\n**정답자 : ${c_anser}**\n**곡 : ${count+1} / ${all_count}**`)
+                    .setImage(`http://img.youtube.com/vi/${yturl}/sddefault.jpg`)
+                    .setFooter(`${time}초뒤에 다음곡으로 넘어갑니다.`)
+                    .setColor('ORANGE');
+                var channelid = data.channelid;
+                var listid = data.listid;
+                var npid = data.npid;
+                setTimeout(async function() {
+                    await play_score(client, message);
+                }, 300);
+                try {
+                    var c = client.channels.cache.get(channelid);
+                    c.messages.fetch(listid).then(m => {
+                        m.edit(list);
+                    });
+                    c.messages.fetch(npid).then(m => {
+                        m.edit(np);
+                    });
+                } catch(err) {}
+            } catch(err) {
+                return await play_end(client, message);
+            }
+            data.count = data.count + 1;
+            await data.save().catch(err => console.log(err));
+            setTimeout(async function() {
+                try {
+                    var c = client.channels.cache.get(data.voicechannelid);
+                } catch(err) {
+                    try {
+                        var c = message.guild.me.voice.channel.id;
+                    } catch(err) {
+                        var c = message.member.voice.channel.id;
+                    }
+                }
+                await db.set(`db.music.${message.guild.id}.user`, {});
+                return await play(client, c, message);
+            }, time * 1000);
+        }
     },
 }
